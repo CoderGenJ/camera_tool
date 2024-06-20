86bf47b8516b4334d2c62464f2863a659e2641f1
zhoulei <zhoulei@senior.auto>
2024-06-20 16:48:42 +0800

    格式化opencv lib 对于提取过程作注释

--- 3rparty/apriltag/example/opencv_demo.cc
+++ 3rparty/apriltag/example/opencv_demo.cc
@@ -25,188 +25,187 @@ of the authors and should not be interpreted as representing official policies,
 either expressed or implied, of the Regents of The University of Michigan.
 */
 
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
 #include "opencv2/opencv.hpp"
 
 extern "C" {
 #include "apriltag.h"
-#include "tag36h11.h"
-#include "tag25h9.h"
+#include "common/getopt.h"
 #include "tag16h5.h"
+#include "tag25h9.h"
+#include "tag36h11.h"
 #include "tagCircle21h7.h"
 #include "tagCircle49h12.h"
 #include "tagCustom48h12.h"
 #include "tagStandard41h12.h"
 #include "tagStandard52h13.h"
-#include "common/getopt.h"
 }
 
 using namespace std;
 using namespace cv;
 
+int main(int argc, char *argv[]) {
+  getopt_t *getopt = getopt_create();
 
-int main(int argc, char *argv[])
-{
-    getopt_t *getopt = getopt_create();
+  getopt_add_bool(getopt, 'h', "help", 0, "Show this help");
+  getopt_add_int(getopt, 'c', "camera", "0", "camera ID");
+  getopt_add_bool(getopt, 'd', "debug", 0, "Enable debugging output (slow)");
+  getopt_add_bool(getopt, 'q', "quiet", 0, "Reduce output");
+  getopt_add_string(getopt, 'f', "family", "tag36h11", "Tag family to use");
+  getopt_add_int(getopt, 't', "threads", "1", "Use this many CPU threads");
+  getopt_add_double(getopt, 'x', "decimate", "2.0",
+                    "Decimate input image by this factor");
+  getopt_add_double(getopt, 'b', "blur", "0.0", "Apply low-pass blur to input");
+  getopt_add_bool(getopt, '0', "refine-edges", 1,
+                  "Spend more time trying to align edges of tags");
 
-    getopt_add_bool(getopt, 'h', "help", 0, "Show this help");
-    getopt_add_int(getopt, 'c', "camera", "0", "camera ID");
-    getopt_add_bool(getopt, 'd', "debug", 0, "Enable debugging output (slow)");
-    getopt_add_bool(getopt, 'q', "quiet", 0, "Reduce output");
-    getopt_add_string(getopt, 'f', "family", "tag36h11", "Tag family to use");
-    getopt_add_int(getopt, 't', "threads", "1", "Use this many CPU threads");
-    getopt_add_double(getopt, 'x', "decimate", "2.0", "Decimate input image by this factor");
-    getopt_add_double(getopt, 'b', "blur", "0.0", "Apply low-pass blur to input");
-    getopt_add_bool(getopt, '0', "refine-edges", 1, "Spend more time trying to align edges of tags");
+  if (!getopt_parse(getopt, argc, argv, 1) || getopt_get_bool(getopt, "help")) {
+    printf("Usage: %s [options]\n", argv[0]);
+    getopt_do_usage(getopt);
+    exit(0);
+  }
 
-    if (!getopt_parse(getopt, argc, argv, 1) ||
-            getopt_get_bool(getopt, "help")) {
-        printf("Usage: %s [options]\n", argv[0]);
-        getopt_do_usage(getopt);
-        exit(0);
-    }
+  cout << "Enabling video capture" << endl;
 
-    cout << "Enabling video capture" << endl;
+  TickMeter meter;
+  meter.start();
 
-    TickMeter meter;
-    meter.start();
+  // Initialize camera
+  VideoCapture cap(getopt_get_int(getopt, "camera"));
+  if (!cap.isOpened()) {
+    cerr << "Couldn't open video capture device" << endl;
+    return -1;
+  }
 
-    // Initialize camera
-    VideoCapture cap(getopt_get_int(getopt, "camera"));
-    if (!cap.isOpened()) {
-        cerr << "Couldn't open video capture device" << endl;
-        return -1;
-    }
+  // Initialize tag detector with options
+  apriltag_family_t *tf = NULL;
+  const char *famname = getopt_get_string(getopt, "family");
+  if (!strcmp(famname, "tag36h11")) {
+    tf = tag36h11_create();
+  } else if (!strcmp(famname, "tag25h9")) {
+    tf = tag25h9_create();
+  } else if (!strcmp(famname, "tag16h5")) {
+    tf = tag16h5_create();
+  } else if (!strcmp(famname, "tagCircle21h7")) {
+    tf = tagCircle21h7_create();
+  } else if (!strcmp(famname, "tagCircle49h12")) {
+    tf = tagCircle49h12_create();
+  } else if (!strcmp(famname, "tagStandard41h12")) {
+    tf = tagStandard41h12_create();
+  } else if (!strcmp(famname, "tagStandard52h13")) {
+    tf = tagStandard52h13_create();
+  } else if (!strcmp(famname, "tagCustom48h12")) {
+    tf = tagCustom48h12_create();
+  } else {
+    printf("Unrecognized tag family name. Use e.g. \"tag36h11\".\n");
+    exit(-1);
+  }
 
-    // Initialize tag detector with options
-    apriltag_family_t *tf = NULL;
-    const char *famname = getopt_get_string(getopt, "family");
-    if (!strcmp(famname, "tag36h11")) {
-        tf = tag36h11_create();
-    } else if (!strcmp(famname, "tag25h9")) {
-        tf = tag25h9_create();
-    } else if (!strcmp(famname, "tag16h5")) {
-        tf = tag16h5_create();
-    } else if (!strcmp(famname, "tagCircle21h7")) {
-        tf = tagCircle21h7_create();
-    } else if (!strcmp(famname, "tagCircle49h12")) {
-        tf = tagCircle49h12_create();
-    } else if (!strcmp(famname, "tagStandard41h12")) {
-        tf = tagStandard41h12_create();
-    } else if (!strcmp(famname, "tagStandard52h13")) {
-        tf = tagStandard52h13_create();
-    } else if (!strcmp(famname, "tagCustom48h12")) {
-        tf = tagCustom48h12_create();
-    } else {
-        printf("Unrecognized tag family name. Use e.g. \"tag36h11\".\n");
-        exit(-1);
-    }
+  // 1.创建detector,以及指出其family
+  apriltag_detector_t *td = apriltag_detector_create();
+  apriltag_detector_add_family(td, tf);
 
+  if (errno == ENOMEM) {
+    printf("Unable to add family to detector due to insufficient memory to "
+           "allocate the tag-family decoder with the default maximum hamming "
+           "value of 2. Try choosing an alternative tag family.\n");
+    exit(-1);
+  }
+  // 2.设置一些重要参数
+  td->quad_decimate = getopt_get_double(getopt, "decimate");
+  td->quad_sigma = getopt_get_double(getopt, "blur");
+  td->nthreads = getopt_get_int(getopt, "threads");
+  td->debug = getopt_get_bool(getopt, "debug");
+  td->refine_edges = getopt_get_bool(getopt, "refine-edges");
 
-    apriltag_detector_t *td = apriltag_detector_create();
-    apriltag_detector_add_family(td, tf);
-
-    if (errno == ENOMEM) {
-        printf("Unable to add family to detector due to insufficient memory to allocate the tag-family decoder with the default maximum hamming value of 2. Try choosing an alternative tag family.\n");
-        exit(-1);
-    }
-
-    td->quad_decimate = getopt_get_double(getopt, "decimate");
-    td->quad_sigma = getopt_get_double(getopt, "blur");
-    td->nthreads = getopt_get_int(getopt, "threads");
-    td->debug = getopt_get_bool(getopt, "debug");
-    td->refine_edges = getopt_get_bool(getopt, "refine-edges");
-
-    meter.stop();
-    cout << "Detector " << famname << " initialized in "
-        << std::fixed << std::setprecision(3) << meter.getTimeSec() << " seconds" << endl;
+  meter.stop();
+  cout << "Detector " << famname << " initialized in " << std::fixed
+       << std::setprecision(3) << meter.getTimeSec() << " seconds" << endl;
 #if CV_MAJOR_VERSION > 3
-    cout << "  " << cap.get(CAP_PROP_FRAME_WIDTH ) << "x" <<
-                    cap.get(CAP_PROP_FRAME_HEIGHT ) << " @" <<
-                    cap.get(CAP_PROP_FPS) << "FPS" << endl;
+  cout << "  " << cap.get(CAP_PROP_FRAME_WIDTH) << "x"
+       << cap.get(CAP_PROP_FRAME_HEIGHT) << " @" << cap.get(CAP_PROP_FPS)
+       << "FPS" << endl;
 #else
-    cout << "  " << cap.get(CV_CAP_PROP_FRAME_WIDTH ) << "x" <<
-                    cap.get(CV_CAP_PROP_FRAME_HEIGHT ) << " @" <<
-                    cap.get(CV_CAP_PROP_FPS) << "FPS" << endl;
+  cout << "  " << cap.get(CV_CAP_PROP_FRAME_WIDTH) << "x"
+       << cap.get(CV_CAP_PROP_FRAME_HEIGHT) << " @" << cap.get(CV_CAP_PROP_FPS)
+       << "FPS" << endl;
 #endif
-    meter.reset();
+  meter.reset();
 
-    Mat frame, gray;
-    while (true) {
-        errno = 0;
-        cap >> frame;
-        cvtColor(frame, gray, COLOR_BGR2GRAY);
+  Mat frame, gray;
+  while (true) {
+    errno = 0;
+    cap >> frame;
+    cvtColor(frame, gray, COLOR_BGR2GRAY);
 
-        // Make an image_u8_t header for the Mat data
-        image_u8_t im = {gray.cols, gray.rows, gray.cols, gray.data};
+    // Make an image_u8_t header for the Mat data
+    // 3.将灰色图转换到img
+    image_u8_t im = {gray.cols, gray.rows, gray.cols, gray.data};
+    // 4.对于marker进行检测
+    zarray_t *detections = apriltag_detector_detect(td, &im);
 
-        zarray_t *detections = apriltag_detector_detect(td, &im);
-
-        if (errno == EAGAIN) {
-            printf("Unable to create the %d threads requested.\n",td->nthreads);
-            exit(-1);
-        }
-
-        // Draw detection outlines
-        for (int i = 0; i < zarray_size(detections); i++) {
-            apriltag_detection_t *det;
-            zarray_get(detections, i, &det);
-            line(frame, Point(det->p[0][0], det->p[0][1]),
-                     Point(det->p[1][0], det->p[1][1]),
-                     Scalar(0, 0xff, 0), 2);
-            line(frame, Point(det->p[0][0], det->p[0][1]),
-                     Point(det->p[3][0], det->p[3][1]),
-                     Scalar(0, 0, 0xff), 2);
-            line(frame, Point(det->p[1][0], det->p[1][1]),
-                     Point(det->p[2][0], det->p[2][1]),
-                     Scalar(0xff, 0, 0), 2);
-            line(frame, Point(det->p[2][0], det->p[2][1]),
-                     Point(det->p[3][0], det->p[3][1]),
-                     Scalar(0xff, 0, 0), 2);
-
-            stringstream ss;
-            ss << det->id;
-            String text = ss.str();
-            int fontface = FONT_HERSHEY_SCRIPT_SIMPLEX;
-            double fontscale = 1.0;
-            int baseline;
-            Size textsize = getTextSize(text, fontface, fontscale, 2,
-                                            &baseline);
-            putText(frame, text, Point(det->c[0]-textsize.width/2,
-                                       det->c[1]+textsize.height/2),
-                    fontface, fontscale, Scalar(0xff, 0x99, 0), 2);
-        }
-        apriltag_detections_destroy(detections);
-
-        imshow("Tag Detections", frame);
-        if (waitKey(30) >= 0)
-            break;
+    if (errno == EAGAIN) {
+      printf("Unable to create the %d threads requested.\n", td->nthreads);
+      exit(-1);
     }
 
-    apriltag_detector_destroy(td);
+    // Draw detection outlines
+    // 5.对于检测结果进行解析
+    for (int i = 0; i < zarray_size(detections); i++) {
+      apriltag_detection_t *det;
+      zarray_get(detections, i, &det);
+      line(frame, Point(det->p[0][0], det->p[0][1]),
+           Point(det->p[1][0], det->p[1][1]), Scalar(0, 0xff, 0), 2);
+      line(frame, Point(det->p[0][0], det->p[0][1]),
+           Point(det->p[3][0], det->p[3][1]), Scalar(0, 0, 0xff), 2);
+      line(frame, Point(det->p[1][0], det->p[1][1]),
+           Point(det->p[2][0], det->p[2][1]), Scalar(0xff, 0, 0), 2);
+      line(frame, Point(det->p[2][0], det->p[2][1]),
+           Point(det->p[3][0], det->p[3][1]), Scalar(0xff, 0, 0), 2);
 
-    if (!strcmp(famname, "tag36h11")) {
-        tag36h11_destroy(tf);
-    } else if (!strcmp(famname, "tag25h9")) {
-        tag25h9_destroy(tf);
-    } else if (!strcmp(famname, "tag16h5")) {
-        tag16h5_destroy(tf);
-    } else if (!strcmp(famname, "tagCircle21h7")) {
-        tagCircle21h7_destroy(tf);
-    } else if (!strcmp(famname, "tagCircle49h12")) {
-        tagCircle49h12_destroy(tf);
-    } else if (!strcmp(famname, "tagStandard41h12")) {
-        tagStandard41h12_destroy(tf);
-    } else if (!strcmp(famname, "tagStandard52h13")) {
-        tagStandard52h13_destroy(tf);
-    } else if (!strcmp(famname, "tagCustom48h12")) {
-        tagCustom48h12_destroy(tf);
+      stringstream ss;
+      ss << det->id;
+      String text = ss.str();
+      int fontface = FONT_HERSHEY_SCRIPT_SIMPLEX;
+      double fontscale = 1.0;
+      int baseline;
+      Size textsize = getTextSize(text, fontface, fontscale, 2, &baseline);
+      putText(frame, text,
+              Point(det->c[0] - textsize.width / 2,
+                    det->c[1] + textsize.height / 2),
+              fontface, fontscale, Scalar(0xff, 0x99, 0), 2);
     }
+    // 6.清空detection
+    apriltag_detections_destroy(detections);
 
+    imshow("Tag Detections", frame);
+    if (waitKey(30) >= 0)
+      break;
+  }
+  // 7.清空td
+  apriltag_detector_destroy(td);
 
-    getopt_destroy(getopt);
+  if (!strcmp(famname, "tag36h11")) {
+    tag36h11_destroy(tf);
+  } else if (!strcmp(famname, "tag25h9")) {
+    tag25h9_destroy(tf);
+  } else if (!strcmp(famname, "tag16h5")) {
+    tag16h5_destroy(tf);
+  } else if (!strcmp(famname, "tagCircle21h7")) {
+    tagCircle21h7_destroy(tf);
+  } else if (!strcmp(famname, "tagCircle49h12")) {
+    tagCircle49h12_destroy(tf);
+  } else if (!strcmp(famname, "tagStandard41h12")) {
+    tagStandard41h12_destroy(tf);
+  } else if (!strcmp(famname, "tagStandard52h13")) {
+    tagStandard52h13_destroy(tf);
+  } else if (!strcmp(famname, "tagCustom48h12")) {
+    tagCustom48h12_destroy(tf);
+  }
 
-    return 0;
+  getopt_destroy(getopt);
+
+  return 0;
 }
